bc44c754f1c9bad23dfe88e25c111fb39b2e123e
ShahadathAlam <shahadathalam@ymail.com>
2025-01-09 23:57:55 +0600

    First Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,48 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 15 Meta Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-1`}
+        >
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,10 @@
+function Page() {
+  return (
+    <div>
+      <h1 className="head-text">Hello, World!</h1>
+      <p>This is a React component.</p>
+    </div>
+  );
+}
+
+export default Page;
