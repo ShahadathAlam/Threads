8c9165a5c4599174280164232985b2fa99f0f841
ShahadathAlam <shahadathalam@ymail.com>
2025-01-12 23:55:21 +0600

    AccountProfile component created and front end part finished successfully

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,109 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Configuration options
+  experimental: {
+    serverActions: {}, // Updated to an empty object
+  },
+  serverExternalPackages: ["mongoose"], // Moved from experimental
+  eslint: {
+    // Allows production builds even if there are ESLint errors
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+
+//   experimental: {
+//     serverActions: true,
+//     serverComponentsExternalPackages: ["mongoose"],
+//   },
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "img.clerk.com",
+//       },
+
+//       {
+//         protocol: "https",
+//         hostname: "images.clerk.dev",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "uploadthing.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "placehold.co",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverActions: true,
+//     serverComponentsExternalPackages: ["mongoose"],
+//   },
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "img.clerk.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "images.clerk.dev",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "uploadthing.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "placehold.co",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
