af756cfb829491092d93cb167d32f49ae079bbb3
ShahadathAlam <shahadathalam@ymail.com>
2025-02-07 20:39:27 +0600

    thread details page created

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,35 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function page({ params }: { params: { id: string } }) {
+  //   const a = await params;
+  //   console.log(a);
+
+  if (!params.id) return null;
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const thread = await fetchThreadById(params.id);
+  return (
+    <section className="relative">
+      <div>
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={user?.id || ""}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={thread.author}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
+      </div>
+    </section>
+  );
+}
