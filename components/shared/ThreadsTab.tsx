c480b8eea925870df5200f16d2e07304cbeadb5d
ShahadathAlam <shahadathalam@ymail.com>
2025-04-12 23:16:25 +0600

    feat: add fetchUserPosts server action to retrieve user threads

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+export default async function ThreadsTab({
+  currentUserId,
+  accountId,
+  accountType,
+}: Props) {
+  // TODO: Fetch profile threads
+  return <section>THreadsTab</section>;
+}
