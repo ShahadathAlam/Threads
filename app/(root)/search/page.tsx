e5fdf19011a26c889767fc1382d00fd4d95b3904
ShahadathAlam <shahadathalam@ymail.com>
2025-04-20 20:28:18 +0600

    UserCard component created

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,5 @@
-import { fetchUser } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 
 import { redirect } from "next/navigation";
@@ -13,9 +14,37 @@ export default async function Page() {
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   // TODO: fetch all users
+
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search Bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
 }
