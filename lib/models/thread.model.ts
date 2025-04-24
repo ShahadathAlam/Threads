6440dcdc20751191782c148db5375013e8b138de
ShahadathAlam <shahadathalam@ymail.com>
2025-04-24 22:44:43 +0600

    need to deploy

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 
 const threadSchema = new mongoose.Schema({
-  text: { type: String, required: true },
+  text: {
+    type: String,
+    required: true,
+  },
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-
   community: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Community",
-    // required: true,
-    required: false,
   },
-
-  createdAt: { type: Date, default: Date.now },
-  parentId: { type: String },
-
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  parentId: {
+    type: String,
+  },
   children: [
     {
       type: mongoose.Schema.Types.ObjectId,
